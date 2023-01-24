c4f03480d19b6a2934374dad6d79ba9cfaf4120b
shawmonika616ms <shawmonika616ms@gmail.com>
2023-01-24 10:40:33 +0530

    coding ninjas NinjaCompetition problem solved

---
+++ coding-ninja/NinjaCompetition.cpp
@@ -0,0 +1,47 @@
+//
+// Created by shawm on 24-01-2023.
+//
+#include <iostream>
+
+using namespace std;
+
+bool isPossible(int n)
+{
+    int even, odd;
+  for(int i = 1; i <= n; i++)
+  {
+      if(n % i == 0)
+      {
+          cout << i << " ";
+          if(i % 2 == 0)
+          {
+              even++;
+          }
+          else
+          {
+              odd++;
+          }
+      }
+  }
+  cout <<"count are" << endl;
+  cout << even << odd << endl;
+  if(even == odd)
+  {
+      return true;
+  }
+  else
+  {
+      return false;
+  }
+}
+
+int main()
+{
+    int num;
+    cout << "enter num";
+    cin>>num;
+    bool result;
+    result = isPossible(num);
+    cout <<result;
+}
+
--- coding-ninja/cmake-build-debug/cmake_install.cmake
+++ coding-ninja/cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: D:/C-Online-Compiler/coding-ninja
+# Install script for directory: D:/online-compiler/coding-ninja
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "D:/C-Online-Compiler/coding-ninja/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "D:/online-compiler/coding-ninja/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
