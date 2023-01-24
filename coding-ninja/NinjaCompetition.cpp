8c86ad61387a9af6d6029d411beeebc031bcced1
shawmonika616ms <shawmonika616ms@gmail.com>
2023-01-24 10:44:36 +0530

    initialization is must in c++

--- coding-ninja/NinjaCompetition.cpp
+++ coding-ninja/NinjaCompetition.cpp
@@ -7,32 +7,33 @@ using namespace std;
 
 bool isPossible(int n)
 {
-    int even, odd;
-  for(int i = 1; i <= n; i++)
-  {
+    int even = 0, odd = 0;
+    for(int i = 1; i <= n; i++)
+    {
       if(n % i == 0)
-      {
+       {
           cout << i << " ";
           if(i % 2 == 0)
-          {
+            {
               even++;
-          }
+            }
           else
-          {
+            {
               odd++;
-          }
+            }
+       }
+    }
+
+    cout <<"count are" << endl;
+    cout << even << odd << endl;
+    if(even == odd)
+      {
+          return true;
       }
-  }
-  cout <<"count are" << endl;
-  cout << even << odd << endl;
-  if(even == odd)
-  {
-      return true;
-  }
-  else
-  {
-      return false;
-  }
+    else
+      {
+          return false;
+      }
 }
 
 int main()
